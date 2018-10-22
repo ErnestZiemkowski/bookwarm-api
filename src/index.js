6b2b7a930cdea8b0de975dd084a843c92ec78c01
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-10-22 22:31:45 +0200

    JWTAuth setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import express from'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import auth from './routes/auth.js';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import Promise from 'bluebird';
 
 const app = express();
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: 'Invalid credentials!' } });
+dotenv.config();
+app.use(bodyParser.json());
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.connection.once('open', () => {
+    console.log('connected to MLab');
 });
+
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
