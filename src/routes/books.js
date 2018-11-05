21be94db24a4067f37c9a773ea3c16571ff215b3
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-11-05 23:47:53 +0100

    Add new book form and change its cover

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-const router = express.Route();
+const router = express.Router();
 
 router.get('/search', (req, res) => {
   res.json({
@@ -10,8 +10,8 @@ router.get('/search', (req, res) => {
         title: "1984",
         authors: "Orwell",
         covers: [
-          "https://images.gr-assetes.com/books/13489905661/5470.jpg",
-          "https://images.gr-assetes.com/books/15046119571/9577857.jpg",
+          "https://images.gr-assets.com/books/1532714506l/40961427.jpg",
+          "https://images.gr-assets.com/books/1327144697l/3744438.jpg",
         ],
         pages: 198
       },
@@ -20,8 +20,8 @@ router.get('/search', (req, res) => {
         title: "Three Men in a Boat",
         authors: "Jerome K. Jerome",
         covers: [
-          "https://images.gr-assetes.com/books/13927916561/4921.jpg",
-          "https://images.gr-assetes.com/books/13120368781/627830.jpg",
+          "https://images.gr-assets.com/books/1392791656l/4921.jpg",
+          "https://images.gr-assets.com/photos/1344892787p8/556276.jpg",
         ],
         pages: 256
       }
