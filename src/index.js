7a0ab5b9af1363458f0efe1a8002380f7b8d4ed1
e.ziemkowski <e.ziemkowski@swresearch.pl>
2018-11-05 16:16:02 +0100

    fetching books from API

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import Promise from 'bluebird';
 import auth from './routes/auth.js';
 import users from './routes/users.js';
+import books from "./routes/books.js";
 
 const app = express();
 
@@ -21,6 +22,7 @@ mongoose.connection.once('open', () => {
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);
+app.use('/api/books', books);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
